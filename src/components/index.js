2f74d2e8f8fa8ea893d7ba54fc6df0c3054b35a3
Tarun Vashishtha <65185652+tarunvashishth@users.noreply.github.com>
2023-02-05 21:22:21 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+export { default as Feed } from './Feed';
+export { default as Header } from './Header';
+export { default as LeftNav } from './LeftNav';
+export { default as LeftNavMenuItem } from './LeftNavMenuItem';
+export { default as SearchResult } from './SearchResult';
+export { default as SearchResultVideoCard } from './SearchResultVideoCard';
+export { default as SuggestionVideoCard } from './SuggestionVideoCard';
+export { default as VideoCard } from './VideoCard';
+export { default as VideoDetails } from './VideoDetails';
